a79824f91c74f7d50aeccf995a0549a3bbbd3ced
Bailey Nepe <bailey.nepe@numberworks.com>
2023-05-02 17:09:13 +1200

    add colors to theme

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -1,6 +1,24 @@
+const greys = {
+  100: '#f7fafc',
+  200: '#edf2f7',
+  300: '#e2e8f0',
+  400: '#cbd5e0',
+  500: '#a0aec0',
+  600: '#718096',
+  700: '#4a5568',
+  800: '#2d3748',
+  900: '#1a202c',
+  1000: '#111827',
+};
+
 export const theme = {
   colors: {
     primary: '#0070f3',
     white: '#ffffff',
+    greys,
+    accent: {
+      foreground: '#8c99e3',
+      background: '#5562ae',
+    },
   },
 };
--- src/components/ui/molecules/InfoBlock/index.tsx
+++ src/components/ui/molecules/InfoBlock/index.tsx
@@ -1,11 +1,66 @@
 import { useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { FiArrowRight } from 'react-icons/fi';
+import styled from 'styled-components';
 
-import styles from './infoBlock.module.css';
 // import ContactModal from './modals/ContactModal';
 
 // import VideoModal from './modals/VideoModal';
+
+const StyledCard = styled(Card)`
+  background: ${({ theme }) => theme.colors.greys[1000]};
+`;
+
+const CardTitle = styled(Card.Title)`
+  color: ${({ theme }) => theme.colors.accent.foreground};
+  padding: 1rem;
+  font-size: 1rem;
+`;
+
+const Icon = styled.span`
+  font-size: 1.5rem;
+`;
+
+const IconText = styled.span`
+  margin-left: 0.5rem;
+`;
+
+const CardBody = styled(Card.Body)`
+  color: ${({ theme }) => theme.colors.greys[200]};
+  padding: 0 1rem 1rem;
+`;
+
+const BodyTitle = styled.div`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 0.2rem;
+`;
+
+const CardFooter = styled(Card.Footer)`
+  display: flex;
+  justify-content: right;
+  color: ${({ theme }) => theme.colors.white};
+  padding: 0rem 1rem 1rem;
+`;
+
+const StyledButton = styled(Button)`
+  background-color: unset;
+  color: ${({ theme }) => theme.colors.accent.foreground};
+  border-color: ${({ theme }) => theme.colors.accent.background};
+
+  &:hover {
+    background-color: unset;
+    color: ${({ theme }) => theme.colors.accent.foreground};
+    border-color: ${({ theme }) => theme.colors.accent.foreground};
+    transition: 250ms ease-in-out;
+  }
+`;
+
+const HorizontalLine = styled.hr`
+  color: ${({ theme }) => theme.colors.greys[800]};
+  width: 100%;
+`;
 
 const InfoBlock = ({
   icon,
@@ -25,27 +80,27 @@ const InfoBlock = ({
 }) => {
   const [modalShow, setModalShow] = useState(false);
   return (
-    <Card className={styles.card}>
-      {/* <Card.Title className={styles.title}>
-        <span className={styles.icon}>{icon}</span>
-        <span className={styles['icon-text']}>{iconText}</span>
-      </Card.Title>
-      <Card.Body className={styles.body}>
-        <div className={styles['body-title']}>{title}</div>
+    <StyledCard>
+      <CardTitle>
+        <Icon>{icon}</Icon>
+        <IconText>{iconText}</IconText>
+      </CardTitle>
+      <CardBody>
+        <BodyTitle>{title}</BodyTitle>
         {text}
-      </Card.Body>
-      <hr style={{ color: 'gray', width: '100%' }} />
-      <Card.Footer className={styles.footer}>
-        <Button className={styles.button} onClick={setModalShow}>
+      </CardBody>
+      <HorizontalLine />
+      <CardFooter>
+        <StyledButton onClick={() => setModalShow(true)}>
           {link.title} <FiArrowRight />
-        </Button>
-        {link.link ? (
+        </StyledButton>
+        {/* {link.link ? (
           <VideoModal show={modalShow} setModalShow={setModalShow} src={link.link} title={title} onHide={() => setModalShow(false)} />
         ) : (
           <ContactModal show={modalShow} setModalShow={setModalShow} title={title} onHide={() => setModalShow(false)} />
-        )}
-      </Card.Footer> */}
-    </Card>
+        )} */}
+      </CardFooter>
+    </StyledCard>
   );
 };
 
